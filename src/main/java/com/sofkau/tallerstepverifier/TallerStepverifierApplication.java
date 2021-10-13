1cde1dcd0f7d96877ccfd31575049e2f492762f4
Joe Watson SBF <joewatson40@hotmail.com>
2021-10-13 02:36:24 -0500

    first commit

---
+++ src/main/java/com/sofkau/tallerstepverifier/TallerStepverifierApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.tallerstepverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerStepverifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerStepverifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/tallerstepverifier/TallerStepverifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.tallerstepverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerStepverifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
